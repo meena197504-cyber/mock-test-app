ecbe6697433ba9a3e4fae27e88dc268b2b70889a
meena197504-cyber <meena197504@gmail.com>
2026-06-17 22:20:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // --- CONFIGURATION ---
-const API_URL = "https://script.google.com/macros/s/AKfycbzb9Id8SvPUhNzARHGUU5PHT_0UuzLtphyeuwSV4OS_UJAZiTdjql2b82ic_ovR1clEBw/execL"; // PASTE YOUR URL HERE
+const API_URL = "https://script.google.com/macros/s/AKfycbzfpHgMS33n2RaiPWxE73KVzay56-f9y5EXmamXcHNEJiGrIuygFe2IJVQ3b-VRiAiauw/exec"; // PASTE YOUR URL HERE
 const RAZORPAY_KEY = "rzp_test_T1D1B9oUvXaqs2"; // PASTE YOUR RAZORPAY KEY HERE
 const MAX_ATTEMPTS = 5;
 
